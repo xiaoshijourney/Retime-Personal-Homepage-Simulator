6f6b6901e82ef2bcce4ec7ffee04b6505f5bb7d9
xiaoshijourney <xiaoshijourney@163.com>
2026-07-04 01:36:33 +0800

    1

--- android/app/src/main/kotlin/com/xiaoshi/retime/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.xiaoshi.retime
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/utils/secret.example.dart
+++
@@ -1,2 +0,0 @@
-// 复制此文件为 secret.dart，替换为你的密钥
-const secretKey = 'your_secret_key_here';
